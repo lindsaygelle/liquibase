8a990ce1ab0f78323d054eff6c0bff1cb8bc545e
lindsaygelle <lindsay.gelle@gmail.com>
2023-05-08 23:52:41 +1000

    Dev

--- src/constraint.py
+++ src/constraint.py
@@ -100,3 +100,30 @@ class AddPrimaryKey(TypedDict):
     """
 
     addPrimaryKey: PrimaryKey
+
+
+class UniqueConstraint(TypedDict):
+    """Permitted attributes for `AddUniqueConstraint.addUniqueConstraint`."""
+
+    catalogName: str
+    clustered: bool
+    columnNames: str
+    constraintName: str
+    deferrable: bool
+    disabled: bool
+    forIndexCatalogName: str
+    forIndexName: str
+    forIndexSchemaName: str
+    initiallyDeferred: bool
+    schemaName: str
+    tableName: str
+    tablespace: str
+    validate: bool
+
+
+class AddUniqueConstraint(TypedDict):
+    """Permitted attributes for change
+    [addUniqueConstraint](https://docs.liquibase.com/change-types/add-primary-key.html).
+    """
+
+    addUniqueConstraint: UniqueConstraint
